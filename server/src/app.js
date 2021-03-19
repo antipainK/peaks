ce5ca1adc3579a05d93c5e4c7b1f6d1d53fbbb07
Mateusz Benecki <benecki.mateusz@gmail.com>
2021-03-19 19:08:06 +0100

    Initial server setup.

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const buildPath = path.join(__dirname, '../build');
+
+app.use(cors());
+app.use(express.static('build'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
+  });
+}
+
+module.exports = app;
