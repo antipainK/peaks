9e18370f3dc6749f05208c98237601596038281a
GabenRulez <45923220+GabenRulez@users.noreply.github.com>
2021-03-29 00:09:38 +0200

    First init. Added logging in and logging out handlers, as well as 'time-out' logout handler.

--- server/src/server.js
+++ server/src/server.js
@@ -23,3 +23,65 @@ const httpServer = http.createServer(app);
 httpServer.listen({ port: config.PORT }, () =>
   console.log(`Server running at port ${config.PORT}`)
 );
+
+
+const { OAuth2Client } = require('google-auth-library');
+const { knex } = require('./db/connection');
+const oauthClient = new OAuth2Client(config.GOOGLE_OAUTH_ID)
+
+
+// Te komendy użyłem na dockerze, nie jestem pewny, czy trzeba je gdzieś wpisać w jakiś plik config
+// docker-compose -f docker-compose.dev.yml exec server npm install google-auth-library
+// docker-compose -f docker-compose.dev.yml exec server npm install express-session cookie-parser
+
+
+
+// This middleware makes sure, we get access to user body, when processing his request
+app.use(async (req, res, next) => {
+  const user = await knex('users').where('id', req.session.userId).first()
+  req.user = user
+  next()
+})
+
+
+// LOGIN
+app.post('/api/v1/auth/google', async (req, res) =>{
+
+  const {token} = req.body
+  const ticket = await clientInformation.verifyIdToken({
+    idToken: token,
+    audience: config.GOOGLE_OAUTH_ID
+  });
+  const {name, email, picture} = ticket.getPayload();
+
+  return knex('users').insert({
+    email: email,
+    displayName: name
+
+  }).then(()=>{
+    req.session.userId = knex('users').select({id: 'id', email: 'email'}).where('email', email).first().select('id')
+
+    console.log("name: " + name + "; email: " + email + "; picture: " + picture)
+    res.status(201).json(user)
+
+  }).catch(() => {
+    res.status(500).json({status: "failure"})
+  })
+})
+
+
+// LOGOUT
+app.delete('/api/v1/auth/logout', async (req, res) => {
+  return req.session.destroy().then(()=>{
+    res.status(200).json({
+      status: 'success',
+      message: `You've been logged out successfully!`
+    })
+  }).catch(()=>{
+    res.status(500).json({
+      status: 'failure',
+      message: `There's been a problem during logging you out. Contact developers.`
+    })
+  })
+})
+
