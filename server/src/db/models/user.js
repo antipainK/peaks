6b5ce28ad5837a7ee8376b8889288b5af4ccff95
pawek001 <58525337+pawek001@users.noreply.github.com>
2021-03-28 13:12:13 +0200

    Added query and migration ( NEEDS REVIEW)

--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -1,9 +1,25 @@
 const { Model } = require('objection');
+const knex = require('knex');
 
 class User extends Model {
   static get tableName() {
     return 'users';
   }
+}
+
+User.findUserById = async function(id){
+  return knex('users').where('id', id)
+  .select('email','displayName');
+}
+
+User.modifyUser = async function(id, email, displayName){
+  knex('users')
+  .where('id', id)
+  .update({
+    email: email,
+    displayName: displayName
+  });
+  return "Mo";
 }
 
 module.exports = User;
--- server/src/graphql/resolvers/user.js
+++ server/src/graphql/resolvers/user.js
@@ -5,6 +5,9 @@ const userResolvers = {
     me: (parent, args, ctx) => {
       return { email: 'test@example.com', displayName: 'marian' };
     },
+    user: async (parent, {id}, {user}) =>{
+      return user.findUserById(id);
+    },
     users: async (parent, args, ctx) => {
       const users = await User.query();
       return users;
@@ -12,4 +15,12 @@ const userResolvers = {
   },
 };
 
-module.exports = userResolvers;
+const userModifiers ={
+  Mutation:{
+    user: async(parent, {id, email, displayName}, {user}) =>{
+      return user.modifyUser(id, email,displayName);
+    }
+  }
+};
+
+module.exports = [userResolvers, userModifiers];
