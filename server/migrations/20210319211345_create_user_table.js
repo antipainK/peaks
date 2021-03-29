ca0491ebe8318a8aebff8421a8385c91f654fa2c
GabenRulez <45923220+GabenRulez@users.noreply.github.com>
2021-03-29 22:21:39 +0200

    Moved migration change to new migration.
    As suggested here: https://github.com/GabenRulez/io-project/pull/16#discussion_r603566355

--- server/migrations/20210319211345_create_user_table.js
+++ server/migrations/20210319211345_create_user_table.js
@@ -5,12 +5,6 @@ module.exports.up = async (knex) => {
     table.string('displayName').notNullable();
     table.string('city').notNullable().defaultTo('');
     table.string('contact').notNullable().defaultTo('');
-    table
-      .string('photoUrl')
-      .notNullable()
-      .defaultTo(
-        'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'
-      );
   });
 };
 
