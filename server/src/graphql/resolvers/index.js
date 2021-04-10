83d1d015b10675d6be65b1f4818cf6b1d9893d99
GabenRulez <45923220+GabenRulez@users.noreply.github.com>
2021-04-11 00:01:22 +0200

    Added `sendMessage` (working~!)

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,5 +1,6 @@
 const userResolvers = require('./user');
 const peakResolvers = require('./peak');
 const chatResolvers = require('./chat');
+const messageResolvers = require('./message');
 
-module.exports = [userResolvers, peakResolvers, chatResolvers];
+module.exports = [userResolvers, peakResolvers, chatResolvers, messageResolvers];
--- server/src/graphql/resolvers/message.js
+++ server/src/graphql/resolvers/message.js
@@ -1,6 +1,15 @@
 const Message = require('../../db/models/message');
+const User = require('../../db/models/user');
 
 const messageResolvers = {
+  Message: {
+    user: async (parent, args, ctx) => {
+      return await parent.$relatedQuery('user');
+    },
+    chat: async (parent, args, ctx) => {
+      return await parent.$relatedQuery('chat');
+    }
+  },
   Query: {
     messages: async (parent, { chatId }, ctx) => {
       const messages = await Message.query().where('chatId', '=', chatId);
@@ -11,6 +20,16 @@ const messageResolvers = {
       }
     },
   },
+  Mutation: {
+    sendMessage: async (parent, { chatId, userId, message }, ctx) => {
+      const messageObject = await Message.query().insert({
+        userId: userId,
+        chatId: chatId,
+        content: message
+      }).returning('*');
+      return messageObject;
+    }
+  }
 };
 
 module.exports = messageResolvers;
