42cb98555c1392bd8b627b22c6086a2ac9ae14cb
GabenRulez <45923220+GabenRulez@users.noreply.github.com>
2021-04-11 20:37:15 +0200

    (re)Added `id` to `userChats` to be compliant with project convention.

--- server/migrations/20210402074331_create_userchats_table.js
+++ server/migrations/20210402074331_create_userchats_table.js
@@ -1,8 +1,9 @@
 module.exports.up = async (knex) => {
   await knex.schema.createTable('userChats', (table) => {
+    table.uuid('id').defaultTo(knex.raw('(gen_random_uuid())')).primary();
     table.uuid('userId').references('id').inTable('users').onDelete('CASCADE');
     table.uuid('chatId').references('id').inTable('chats').onDelete('CASCADE');
-    table.primary(['userId', 'chatId']);
+    table.unique(['userId', 'chatId']);
   });
 };
 
