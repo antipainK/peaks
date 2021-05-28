b0b8ae49871258e5a50b0e1ee064ceeecfb2f814
Mateusz Benecki <benecki.mateusz@gmail.com>
2021-05-28 15:57:58 +0200

    Add migration, model, schema and resolvers for the followers feature

--- server/src/graphql/resolvers/user.js
+++ server/src/graphql/resolvers/user.js
@@ -28,6 +28,14 @@ const userResolvers = {
     tracks: async (parent, args, ctx) => {
       return await parent.$relatedQuery('tracks');
     },
+
+    followers: async (parent, args, ctx) => {
+      return await parent.$relatedQuery('followers');
+    },
+
+    following: async (parent, args, ctx) => {
+      return await parent.$relatedQuery('following');
+    },
   },
 
   Query: {
@@ -62,6 +70,50 @@ const userResolvers = {
         .returning('*');
 
       return updatedUser;
+    },
+
+    followUser: async (parent, { id }, { userId }) => {
+      if (!userId) throw new AuthenticationError('Not authenticated');
+      const user = await User.query()
+        .findById(userId)
+        .withGraphFetched('following');
+
+      const userToFollow = await User.query().findById(id);
+
+      if (!userToFollow) {
+        return new Error('User not found');
+      }
+
+      const alreadyFollowing = user.following.find(
+        (followedUser) => followedUser.id === id
+      );
+
+      if (alreadyFollowing) {
+        return new Error('You are already following a user with this ID');
+      }
+
+      await user.$relatedQuery('following').relate(id);
+
+      return user;
+    },
+
+    unfollowUser: async (parent, { id }, { userId }) => {
+      if (!userId) throw new AuthenticationError('Not authenticated');
+      const user = await User.query()
+        .findById(userId)
+        .withGraphFetched('following');
+
+      const followRelation = await user.following.find(
+        (followedUser) => followedUser.id === id
+      );
+
+      if (!followRelation) {
+        return new Error('You are not following any user with this ID');
+      }
+
+      await user.$relatedQuery('following').unrelate().where('toId', id);
+
+      return user;
     },
   },
 };
