d08f5287f296342a8e32beaf8de761be001477f5
Wojciech Kosztyła <wojtex1221@gmail.com>
2021-05-30 14:34:37 +0200

    Fixed wrong db queries.

--- server/src/graphql/resolvers/statistics.js
+++ server/src/graphql/resolvers/statistics.js
@@ -16,8 +16,12 @@ const statisticsResolvers = {
     finishedPeaks: async (parent, args, ctx) => {
       const userPeaksDistinctCount = await Expedition.query()
         .where('date', '<', new Date())
-        .whereExists(Track.query().where('userId', '=', parent.id))
+        .whereIn(
+          'id',
+          Track.query().where('userId', '=', parent.id).select('expeditionId')
+        )
         .countDistinct('peakId');
+
       return userPeaksDistinctCount[0].count;
     },
     allPeaks: async (parent, args, ctx) => {
@@ -31,7 +35,10 @@ const statisticsResolvers = {
       const userExpeditionCount = await Expedition.query()
         .count()
         .where('date', '<', new Date())
-        .whereExists(Track.query().where('userId', '=', parent.id));
+        .whereIn(
+          'id',
+          Track.query().where('userId', '=', parent.id).select('expeditionId')
+        );
       return userExpeditionCount[0].count;
     },
   },
