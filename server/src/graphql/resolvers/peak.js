30314b97882ed11954bb67bea88a4c9d89092128
Paweł Kiełbasa <305323@student.agh.edu.pl>
2021-06-12 14:25:20 +0200

    First try to small statistics for peaks

--- server/src/graphql/resolvers/peak.js
+++ server/src/graphql/resolvers/peak.js
@@ -1,10 +1,33 @@
 const Peak = require('../../db/models/peak');
+const Expedition = require('../../db/models/expedition');
+const UserAchievemt = require('../../db/models/userAchievement');
 
 const peakResolvers = {
   Peak: {
     expeditions: async (parent, { id }, ctx) => {
       return await parent.$relatedQuery('expeditions').orderBy('date', 'desc');
     },
+    peopleFinished: async (parent, { id }, ctx) => {
+      return await Expedition.query()
+        .join('tracks', 'expeditions.id', 'tracks.expeditionId')
+        .where('stoppedAt', '<', new Date())
+        .where('peakId', id)
+        .select('userId')
+        .distinct();
+    },
+    peopleFinishedCount: async (parent, { id }, ctx) => {
+      return await Expedition.query()
+        .join('tracks', 'expeditions.id', 'tracks.expeditionId')
+        .where('stoppedAt', '<', new Date())
+        .where('peakId', id)
+        .countDistinct('userId');
+    },
+    peopleReached: async (parent, { id }, ctx) => {
+      return await UserAchievemt.query()
+        .join('achievemets', 'userAchievements.achievemetId', 'achievements.id')
+        .where({ metaId: id, TYPE: 'PEAK' });
+    },
+    peopleReachedCount: async (parent, { id }, ctx) => {},
   },
   Query: {
     peak: async (parent, { id }, ctx) => {
